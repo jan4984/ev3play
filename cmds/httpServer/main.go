b899020a16da6e7298af6c47217d683b828bc5a3
YD Jiang <jan4984@gmail.com>
2018-08-14 18:06:24 +0800

    add tts for html

--- cmds/httpServer/main.go
+++ cmds/httpServer/main.go
@@ -5,9 +5,11 @@ import (
 	"github.com/jan4984/ev3play"
 	"net/http"
 	"github.com/gorilla/handlers"
+	"os"
 )
 
 func main(){
+	os.Unsetenv("http_proxy")
 	r := mux.NewRouter()
 	ev3play.RegisterMotorHandlers(r.PathPrefix("/motor").Subrouter())
 	ev3play.RegisterSoundHandlers(r.PathPrefix("/sound").Subrouter())
